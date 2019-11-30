4248049efeb3dd32ce8099304d66bed66e905c3c
Mirakul1 <collinsalex50@gmail.com>
2019-11-30 18:00:53 +0100

    all

--- Adminpanel/urls.py
+++ Adminpanel/urls.py
@@ -36,20 +36,10 @@ urlpatterns = [
 ]   
 
 
-# if settings.DEBUG:
-#         urlpatterns += static(settings.MEDIA_URL,
-#                               document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
 
 
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-# router = routers.DefaultRouter()                      # add this
-# router.register(r'posts', views.PostView, 'Crud')     # add this
-        
-# urlpatterns = [
-#     path('admin/', admin.site.urls),           
-#     path('api/', include(router.urls))                # add this
-# ]                              
