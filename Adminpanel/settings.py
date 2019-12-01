88c1fe9f39b2b99872cf00cb48fcf74b50019a7b
Mirakul1 <collinsalex50@gmail.com>
2019-12-01 18:55:57 +0100

    added allowed hosts

--- Adminpanel/settings.py
+++ Adminpanel/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ks@n-91o%)ia0--oazs(4ssgfhld%=g=ay74r8%172l_75dagk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['light-crm.herokuapp.com']
 
 
 # Application definition
